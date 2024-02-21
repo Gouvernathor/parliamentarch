7b02191f3b899ee43fa7e220a2f467263c3c6a1e
ppavia <pierre.pavia@assemblee-nationale.fr>
2024-02-21 17:41:52 +0100

    Add function for seats centers

--- __init__.py
+++ __init__.py
@@ -47,3 +47,49 @@ def get_nrows_from_number_of_seats(nseats:int)->int:
     return i
 
 _cached_get_nrows_from_number_of_seats = functools.cache(get_nrows_from_number_of_seats)
+
+
+def get_seats_centers(nseats:int, nrows:int=None, *, outer_fill_first:bool=False, _seat_radius:float=None)->list[tuple[float, float, float]]:
+    """
+    Returns a list of seat centers as (angle, x, y) tuples.
+    The canvas is assumed to be of 2 in width and 1 in height, with the y axis pointing up.
+    The angle is calculated from the (1., 0.) center of the hemicycle, in radians,
+    with 0° for the leftmost seats, 90° for the center and 180° for the rightmost.
+
+    outer_fill_first is the legacy dense_rows
+    """
+    # _seat_radius is in the same unit as the coordinates
+    # TODO: figure out if _seat_radius is relevant or should be removed
+    
+    if nrows is None:
+        nrows = _cached_get_nrows_from_number_of_seats(nseats)
+    if _seat_radius is None:
+        _seat_radius = 1 / (4*nrows - 2)
+
+    maxed_rows = _cached_get_rows_from_number_of_rows(nrows)
+    maxed_nseats = sum(maxed_rows)
+    filling_ratio = nseats/maxed_nseats
+    
+    positions = []
+    for r in range(1, nrows+1):
+        # row radius : the radius of the circle crossing the center of each seat in the row
+        R = .5 + 2*(r-1)*_seat_radius
+
+        if r == nrows: # if it's the last row
+            # fit all the remaining seats
+            nseats_this_row = nseats-len(positions)
+        else:
+            # fullness of the diagram times the maximum number of seats in the row
+            nseats_this_row = round(filling_ratio * maxed_rows[r-1])
+
+        if nseats_this_row == 1:
+            positions.append((math.pi/2, 1., R))
+        else:
+            for s in range(nseats_this_row):
+                # angle of the seat's position relative to the hemicycle center
+                angle = s * (math.pi-2*math.asin(_seat_radius/R)) / (nseats_this_row-1) + math.asin(_seat_radius/R)
+
+                positions.append((angle, R*math.cos(angle)+1, R*math.sin(angle)))
+    
+    positions.sort(reverse=True)
+    return positions
