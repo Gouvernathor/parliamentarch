c09364006fac32ef96fb72e2a2ef04db6bd431dc
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-04-09 22:11:58 +0200

    Remove useless SVG group

--- src/parliamentarch/svg.py
+++ src/parliamentarch/svg.py
@@ -110,8 +110,7 @@ def _write_svg_header(file: TextIOBase, width: float, height: float) -> None:
 <svg xmlns:svg="http://www.w3.org/2000/svg"
      xmlns="http://www.w3.org/2000/svg" version="1.1"
      width="{width}" height="{height}">
-    <!-- Created with parliamentarch (https://github.com/Gouvernathor/parliamentarch/) -->
-    <g>""")
+    <!-- Created with parliamentarch (https://github.com/Gouvernathor/parliamentarch/) -->""")
 
 def _write_svg_number_of_seats(
         file: TextIOBase,
@@ -120,8 +119,8 @@ def _write_svg_number_of_seats(
         font_size: int,
         ) -> None:
     file.write(f"""
-        <text x="{x}" y="{y}"
-              style="font-size:{font_size}px;font-weight:bold;text-align:center;text-anchor:middle;font-family:sans-serif">{nseats}</text>""")
+    <text x="{x}" y="{y}"
+          style="font-size:{font_size}px;font-weight:bold;text-align:center;text-anchor:middle;font-family:sans-serif">{nseats}</text>""")
 
 def _write_grouped_svg_seats(
         file: TextIOBase,
@@ -151,23 +150,22 @@ def _write_grouped_svg_seats(
             group_border_color = group_border_color.hexcode
 
         file.write(f"""
-        <g style="fill:{group_color}; stroke-width:{group_border_width:.2f}; stroke:{group_border_color}"
-           id="{block_id}">
-            <title>{group.data}</title>""")
+    <g style="fill:{group_color}; stroke-width:{group_border_width:.2f}; stroke:{group_border_color}"
+       id="{block_id}">
+        <title>{group.data}</title>""")
 
         for x, y in seat_centers:
             actual_x = left_margin + canvas_size * x
             actual_y = top_margin + canvas_size * (1 - y)
             actual_radius = seat_actual_radius * canvas_size - group_border_width/2
             file.write(f"""
-            <circle cx="{actual_x:.2f}" cy="{actual_y:.2f}" r="{actual_radius:.2f}"/>""")
+        <circle cx="{actual_x:.2f}" cy="{actual_y:.2f}" r="{actual_radius:.2f}"/>""")
 
         file.write("""
-        </g>""")
+    </g>""")
 
 def _write_svg_footer(file: TextIOBase) -> None:
     file.write("""
-    </g>
 </svg>
 """)
 
