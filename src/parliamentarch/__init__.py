cbf7323ba733381870358a3b584fd3ac683ed675
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-05-20 19:10:28 +0200

    Build the real signature for the main functions
    and remove useless import

--- src/parliamentarch/__init__.py
+++ src/parliamentarch/__init__.py
@@ -1,5 +1,4 @@
 from inspect import signature
-from io import TextIOBase
 
 from .geometry import get_seats_centers
 from .svg import SeatData, dispatch_seats, get_grouped_svg
@@ -7,8 +6,8 @@ from ._util import filter_kwargs, write_from_get
 
 __all__ = ("get_svg_from_attribution", "write_svg_from_attribution", "SeatData")
 
-_GET_SEATS_CENTERS_PARAM_NAMES = {k for k, p in signature(get_seats_centers).parameters.items() if p.kind==p.KEYWORD_ONLY}
-_WRITE_GROUPED_SVG_PARAM_NAMES = {k for k, p in signature(get_grouped_svg).parameters.items() if p.kind==p.KEYWORD_ONLY}
+_GET_SEATS_CENTERS_PARAM_NAMES = {k: p for k, p in signature(get_seats_centers).parameters.items() if p.kind==p.KEYWORD_ONLY}
+_WRITE_GROUPED_SVG_PARAM_NAMES = {k: p for k, p in signature(get_grouped_svg).parameters.items() if p.kind==p.KEYWORD_ONLY}
 
 def get_svg_from_attribution(attrib: dict[SeatData, int], **kwargs) -> str:
     nseats = sum(attrib.values())
@@ -20,5 +19,14 @@ def get_svg_from_attribution(attrib: dict[SeatData, int], **kwargs) -> str:
     results = get_seats_centers(nseats, **get_seats_centers_kwargs)
     seat_centers_by_group = dispatch_seats(attrib, sorted(results, key=results.__getitem__, reverse=True))
     return get_grouped_svg(seat_centers_by_group, results.seat_actual_radius, **write_grouped_svg_kwargs)
+
+_sig = signature(get_svg_from_attribution)
+_attrib_param = _sig.parameters["attrib"]
+get_svg_from_attribution.__signature__ = _sig.replace(parameters=(
+    _attrib_param,
+    *_GET_SEATS_CENTERS_PARAM_NAMES.values(),
+    *_WRITE_GROUPED_SVG_PARAM_NAMES.values()
+))
+del _sig, _attrib_param
 
 write_svg_from_attribution = write_from_get(get_svg_from_attribution)
